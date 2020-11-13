c511a997a141f0210beefa9d0ed3ad8526f3b94c
shishkov <ivan.shishkov.2012@mail.ru>
2020-11-13 20:36:25 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import axios from 'axios'
+import NumberFormat from 'react-number-format'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const method = 'GET'
+const url =
+    'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,0XBTC,1ST,1WO,AAC,ABCC,ABYSS,ZEC,ZIL,YOYOW&tsyms=USD'
+
+function componentWillMount() {
+    axios
+        .request({
+            url,
+            method,
+            responseType: 'blob',
+        })
+        .then(({ data }) => {
+            const downloadUrl = window.URL.createObjectURL(new Blob([data]))
+            const link = document.createElement('a')
+            link.href = downloadUrl
+            link.setAttribute('download', 'file.csv')
+            document.body.appendChild(link)
+            link.click()
+            link.remove()
+        })
 }
 
-export default App;
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            cryptos: [],
+        }
+    }
+
+    componentDidMount() {
+        axios.get(url).then((res) => {
+            const cryptos = res.data
+            this.setState({ cryptos })
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                {Object.keys(this.state.cryptos).map((key) => (
+                    <div id="crypto-container">
+                        <span className="left">{key}</span>
+                        <span className="right">
+                            <NumberFormat
+                                value={this.state.cryptos[key].USD}
+                                displayType={'text'}
+                                decimalPrecision={2}
+                                thousandSeparator={true}
+                                prefix={'$'}
+                            />
+                        </span>
+                    </div>
+                ))}
+                <button onClick={componentWillMount}>Download data</button>
+            </div>
+        )
+    }
+}
+
+export default App
